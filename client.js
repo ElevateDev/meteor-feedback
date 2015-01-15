e1c9d7774a3c31f43e4ad4c79b16974d85314ad5
Jimmie Butler <jimmiebtlr@gmail.com>
2015-01-15 16:25:17 -0600

    Update call to check for vibrate function before use.

--- client.js
+++ client.js
@@ -1,11 +1,14 @@
 Feedback = {
-  sound: {  
+  _vibrate: function( haptic ){
+    if( navigator.vibrate ){
+      navigator.vibrate( haptic );
+    }
   }
 };
 
 
 /*
- * Setup the play function
+ * Setup the play function, dependent on platform
  */
 if( Meteor.isCordova ){
   var mediaSuccess = function () { console.log("playAudio():Audio Success");};
@@ -14,28 +17,28 @@ if( Meteor.isCordova ){
       console.log( err.code );
     };
   Meteor.startup(function(){
-    Feedback.sound._play = function(path){
+    Feedback._play = function(path){
       var my_media = new Media( Meteor.absoluteUrl(path), mediaSuccess, mediaErr );
       my_media.play();
     }
   });
 }else{
-  Feedback.sound._play = function(path){
+  Feedback._play = function(path){
     new Audio(path).play();
   } 
 } 
 
 /*
- * Setup the provide function 
+ * Feedback.provide finds the profile for the feedback, and calls neccessary functions.
  */
 Meteor.startup(function(){
   Feedback.provide = function( profile ){
     var fdbk = this.profiles[profile];
     if( fdbk.vibrate ){
-      navigator.vibrate(fdbk.vibrate);
+      Feedback._vibrate(fdbk.vibrate);
     }
     if( fdbk.sound ){
-      Feedback.sound._play( fdbk.sound );
+      Feedback._play( fdbk.sound );
     }
   }
 });
